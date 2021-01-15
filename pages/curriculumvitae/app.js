07ff6d9c78b661494887981354a326a63ba328b1
nelsonviloriadeguzman <74486967+nelsonviloriadeguzman@users.noreply.github.com>
2021-01-16 04:23:47 +0800

    Add files via upload

--- pages/curriculumvitae/app.js
+++ pages/curriculumvitae/app.js
@@ -20,6 +20,7 @@ new Vue({
             fullName: 'Nelson V. de Guzman',
             mobileNumber: '+63 926 659 3763',
             emailAddress: 'nelsonviloriadeguzman@gmail.com',
+            skypeName: 'live:nelsonviloriadeguzman',
             github: 'nelsonviloriadeguzman.github.io/activities',
             imagePath: 'img/defaultImage.png'
           },
@@ -164,7 +165,7 @@ new Vue({
           window.location.href = '../../index.html'
         },
         
-        printPDF(PrintOrDownload){
+        downloadOrPreviewPDF(downloadOrPreview){
 
           var dd = {
             content: [
@@ -175,7 +176,7 @@ new Vue({
                     layout: 'noBorders',
                 table: {
                     widths:['15%','45%','40%'],
-                  body: [[	{text:'img/defaultImage.png'},
+                  body: [[	{text:''},
                           {
                               style: 'infoStyle',
                                     layout: 'noBorders',
@@ -184,6 +185,7 @@ new Vue({
                                   body: [[{ text: `${this.shortProfile.fullName}
                                                   ${this.shortProfile.mobileNumber}
                                                   ${this.shortProfile.emailAddress}
+                                                  ${this.shortProfile.skypeName}
                                                   ${this.shortProfile.github}`}]]
                                     },
                                   
@@ -194,12 +196,45 @@ new Vue({
                                     widths:['*'],
                                   body: [[{ text: 'Work Experience', style: 'headerStyle',}],
                                           [{ text: '\nOffice Staff / Programmer', style:"miniTitleStyle",}],
-                                          [{ text: 'H.R.D. Singapore Pte., Ltd.\nBlk 3, Cavite Eco Zone II Gen. Trias, Cavite\n(046) 476-0545 / (046) 476-0465 / (02) 857-8210\nJune 2018 - September 2020\n\n'}]
+                                          [{ text: 'H.R.D. Singapore Pte., Ltd.\nBlk 3, Cavite Eco Zone II Gen. Trias, Cavite\n(046) 476-0545 / (046) 476-0465 / (02) 857-8210\nJune 2018 - September 2020\n\n',style: 'defaultTextStyle'}]
                                       ]
                                     },
                                   
                                 }
                               ]]
+                },
+                  
+                },
+
+              //INFORMATION TECHNOLOGY SKILLS
+                {
+                  layout: 'noBorders',
+                  style: 'headerStyle',
+                table: {
+                    widths:['*'],
+                  body: [[{ text: 'Information Technology Skills',}]]
+                },
+                  
+                },
+                {
+                    layout: 'noBorders',
+                table: {
+                    widths:['*','*','*','*'],
+                  body: [[{ text: '\nJavascript Framework ', style:"miniTitleStyle"},
+                          { text: '\nCSS Framework ', style:"miniTitleStyle"},
+                          { text: '\nServer Side / Back End', style:"miniTitleStyle"},
+                          { text: '\nDatabase', style:"miniTitleStyle"},
+                          ],
+                          
+                          [{ text: "Vue.js\n\n", style: 'defaultTextStyle'},
+                          { text: 'Vuetify\nBootstrap\nMaterialize\n\n', style: 'defaultTextStyle'},
+                          { text: 'Node.js\nMagicXPA\nPHP\nc#\n\n', style: 'defaultTextStyle'},
+                          { text: 'MySQL\nCouchDB\nSQL Server\nMS Access\n\n', style: 'defaultTextStyle'},
+                          ],
+                          
+                          [{},{},{},{}]
+                          ],
+                          
                 },
                   
                 },
@@ -219,11 +254,11 @@ new Vue({
                 table: {
                     widths:['*','*'],
                   body: [[{ text: '\nDaily', style:"miniTitleStyle"},
-                          { text: '\nPeriodic/Occational', style:"miniTitleStyle"},
+                          { text: '\nPeriodic/Occasional', style:"miniTitleStyle"},
                           ],
                           
-                          [{ text: "Analyze system flow and the process of all handled system\nModify existing system for improvement\nFixing system troubles\nTracing of missing data or if there's data discrepancy\nMonitor server activities and performace\nAct as support for system user for their system inqueries\nData modification for correction as request by user\nPerforms data extraction as requested by user\n\n"},
-                          { text: 'Update of master tables if needed as users request\nSupport and attends to co-members queries in technial aspects\nSuggest improvement for lessen manual checking\n Develop the approved requested system\nAdminister the newly developed system from trial deployment up to actual impementation\nAttend meeting with requester as part of system development and system modification if needed\nTrainer of newly hired staffs\n\n'},
+                          [{ text: "Analyze system flow and the process of all handled system\nModify existing system for improvement\nFixing system troubles\nTracing of missing data or if there's data discrepancy\nMonitor server activities and performace\nAct as support for system user for their system inqueries\nData modification for correction as request by user\nPerforms data extraction as requested by user\n\n", style: 'defaultTextStyle'},
+                          { text: 'Update of master tables if needed as users request\nSupport and attends to co-members queries in technial aspects\nSuggest improvement for lessen manual checking\n Develop the approved requested system\nAdminister the newly developed system from trial deployment up to actual impementation\nAttend meeting with requester as part of system development and system modification if needed\nTrainer of newly hired staffs\n\n', style: 'defaultTextStyle'},
                           ],
                           
                           [{},{}]
@@ -249,8 +284,8 @@ new Vue({
                   body: [[{ text: '\nNational College of Science and Technology', style:"miniTitleStyle"},
                           { text: '\nManalo Academy School', style:"miniTitleStyle"},
                           ],
-                          [{ text: 'Amafel Building Aguinaldo Highway, Dasmariñas City, Cavite \n (046) 416-NCST (6278) \n B.S in Information Technology \n 2013 - 2017'},
-                          { text: 'Gen. Trias, Cavite \n High School \n 2003 - 2007'},
+                          [{ text: 'Amafel Building Aguinaldo Highway, Dasmariñas City, Cavite \n (046) 416-NCST (6278) \n B.S in Information Technology \n 2013 - 2017', style: 'defaultTextStyle'},
+                          { text: 'Gen. Trias, Cavite \n High School \n 2003 - 2007', style: 'defaultTextStyle'},
                           ],
                           [{},{}]
                           ]
@@ -260,19 +295,23 @@ new Vue({
                 
             ],
             styles: {
+              defaultTextStyle:{
+                fontSize: 10,
+              },
               headerStyle: {
                   borderColor: 'white',
                   fillColor: '#1976D2',
                   color: 'white',
-                fontSize: 14,
+                fontSize: 12,
                 bold: true
               },
               miniTitleStyle: {
-                bold: true
+                bold: true,
+                fontSize: 11,
               },
               infoStyle: {
                   bold: true,
-                  fontSize: 15,
+                  fontSize: 13,
               }
               
             },
@@ -280,7 +319,7 @@ new Vue({
             
           }
 
-          PrintOrDownload === 1 ? createPdf(dd).download('CV - Nelson V. de Guzman') : createPdf(dd).open()
+          downloadOrPreview === 1 ? createPdf(dd).download('CV - Nelson V. de Guzman') : createPdf(dd).open()
       
         }
       }
